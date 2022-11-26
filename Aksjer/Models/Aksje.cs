0eb58f20d27236f751ba0fae872e7d5bbdc7385c
Alexander Hagen Huse <ahh-produktivitet@tutanota.com>
2022-11-26 22:59:52 +0100

    Småkorrigering

--- Aksjer/Models/Aksje.cs
+++ Aksjer/Models/Aksje.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public string Aksjenavn { get; set; } = string.Empty;
         public string Ticker { get; set; } = string.Empty;
-        public double Pris { get; set; }
+        public double Markedspris { get; set; }
         public int TotaltAntallAksjer { get; set; }
         public string Boers { get; set; } = string.Empty;
     }
