b1f4f64f7e8c708774f7013d11659fb9fe67313d
Alexander Hagen Huse <ahh-produktivitet@tutanota.com>
2022-11-26 22:52:32 +0100

    La til service for aksjehandler

--- Aksjer/ClientApp/src/app/services/handle.service.ts
+++ Aksjer/ClientApp/src/app/services/handle.service.ts
@@ -1,3 +1,27 @@
+import {Injectable} from "@angular/core";
+
+
+@Injectable({
+    providedIn: 'root'
+})
+export class HandleService {
+
+    visHandleAksjerVindu = false;
+    
+    constructor() { }
+
+}
+
+
+
+
+
+
+
+
+///////////////////////////
+
+
 /*
 import { Injectable } from '@angular/core';
 
