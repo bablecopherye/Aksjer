1b2f27a4ac67033d55c1d75480a48006c7fe2cd4
Alexander Hagen Huse <ahh-produktivitet@tutanota.com>
2022-11-26 11:42:42 +0100

    Opprettet handle.service.ts for å kunne bruke handle-komponentet på tvers av applikasjonen

---
+++ Aksjer/ClientApp/src/app/services/handle.service.ts
@@ -0,0 +1,33 @@
+/*
+import { Injectable } from '@angular/core';
+
+import { ModalDirective } from 'ng-uikit-pro-standard';
+
+import { NgbModal, ModalDismissReasons } from "@ng-bootstrap/ng-bootstrap";
+
+@Injectable({
+
+    providedIn:'root'
+
+})
+
+export class HandleService {
+
+    yourModal:ModalDirective;
+
+    constructor() { }
+
+    setModal(modal:ModalDirective) {
+
+        this.yourModal=modal;
+
+    }
+
+    visHandleModal() {
+
+        this.yourModal.show();
+
+    }
+
+}
+*/
