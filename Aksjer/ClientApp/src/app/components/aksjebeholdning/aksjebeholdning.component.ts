2418da61be4127d08931f880f190857d271bfe30
Alexander Hagen Huse <ahh-produktivitet@tutanota.com>
2022-11-26 12:25:50 +0100

    Gjorde klar aksjebeholdning.component.ts til å vise fram og laste inn aksjebeholdningen til den akutelle bruker.

---
+++ Aksjer/ClientApp/src/app/components/aksjebeholdning/aksjebeholdning.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit} from "@angular/core";
+import { IAksje } from "src/app/models/aksje";
+import {AksjeService} from "../../services/aksje.service";
+
+@Component({
+    selector: 'aksjebeholdning',
+    templateUrl: './aksjebeholdning.component.html',
+    styleUrls: ['./aksjebeholdning.component.css']
+})
+export class AksjebeholdningComponent implements OnInit {
+
+    constructor() {}
+
+
+
+    ngOnInit() {}
+    
+}
