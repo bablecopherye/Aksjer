da9d0732889fd28183b702099998597f2bbc5693
Alexander Hagen Huse <ahh-produktivitet@tutanota.com>
2022-11-27 17:26:55 +0100

    Småjusteringer og skisser av diagram

--- Aksjer/ClientApp/src/app/components/diagram/diagram.component.ts
+++ Aksjer/ClientApp/src/app/components/diagram/diagram.component.ts
@@ -1,5 +1,5 @@
 ﻿import { Component } from "@angular/core";
-import {ApexChart, ApexNonAxisChartSeries, NgApexchartsModule} from "ng-apexcharts";
+import {ApexChart, ApexNonAxisChartSeries, ApexOptions, NgApexchartsModule} from "ng-apexcharts";
 import { OnInit} from "@angular/core";
 
 @Component({
@@ -7,16 +7,52 @@ import { OnInit} from "@angular/core";
     templateUrl: './diagram.component.html',
     styleUrls: ['./diagram.component.css']
 })
-export class DiagramComponent implements OnInit{
-    
+export class DiagramComponent implements OnInit {
+
     public chartSeries: ApexNonAxisChartSeries = [40, 32, 28, 55];
-    
+
     public chartDetails: ApexChart = {
         type: 'pie',
         toolbar: {
             show: true
         }
+    }
+/*
+    public options: ApexOptions = {
+        series: [44, 55, 41, 17, 15],
+        chart: {
+            type: 'donut',
+        },
+        plotOptions: {
+            pie: {
+                startAngle: -90,
+                endAngle: 90,
+                offsetY: 10
+            }
+        },
+        grid: {
+            padding: {
+                bottom: -80
+            }
+        },
+        responsive: [{
+            breakpoint: 480,
+            options: {
+                chart: {
+                    width: 200
+                },
+                legend: {
+                    position: 'bottom'
+                }
+            }
+        }]
     };
+
+    var chart = new ApexCharts(document.querySelector("#chart"), options);
+    chart.render();
+    
+    public plotOptions: 
+*/
 
     constructor() {
     }
