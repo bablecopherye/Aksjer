21319eb1905abf5e9bc171a41ee651b996305be5
Alexander Hagen Huse <ahh-produktivitet@tutanota.com>
2022-11-27 22:05:06 +0100

    Opprettet skisse til Aksjebeholdning.cs

--- Aksjer/Models/Aksjebeholdning.cs
+++ Aksjer/Models/Aksjebeholdning.cs
@@ -1,7 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Aksjer.Models
 {
     public class Aksjebeholdning
     {
+        public int Id { get; set; }
+        public Aksje Aksje { get; set; }
         
+        [RegularExpression(@"^[0-9]{1,20}$")]
+        public int AntallAksjerEid { get; set; }
+        
+        public double Kostpris { get; set; }
     }
 }
