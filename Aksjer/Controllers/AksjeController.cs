ac3b7d10009babd06797e0155fb53d13e1e21ef9
Alexander Hagen Huse <ahh-produktivitet@tutanota.com>
2022-11-27 21:27:00 +0100

    Fjernet kommentarer

--- Aksjer/Controllers/AksjeController.cs
+++ Aksjer/Controllers/AksjeController.cs
@@ -47,12 +47,6 @@ namespace Aksjer.Controllers
         [HttpGet]
         public async Task<ActionResult> HentAlle()
         {
-            /*
-            if (string.IsNullOrEmpty(HttpContext.Session.GetString(_loggetInn)))
-            {
-                return Unauthorized();
-            }
-            */
             List<Aksje> alleAksjer = await _db.HentAlle();
             return Ok(alleAksjer);
         }
@@ -131,79 +125,3 @@ namespace Aksjer.Controllers
         }
     }
 }
-
-/*
-using System.Collections.Generic;
-using Aksjer.Models;
-using Microsoft.AspNetCore.Mvc;
-
-namespace Aksjer.Controllers
-{
-    [Route("api/[controller]")]
-    [ApiController]
-    public class AksjeController : ControllerBase
-    {
-        private List<Aksje> _alleAksjer = new List<Aksje>();
-
-        [HttpGet]
-        public List<Aksje> Get()
-        {
-            var aksjeApple = new Aksje
-            {
-                Id = 1,
-                Aksjenavn = "Apple",
-                Ticker = "AAPL",
-                Markedspris = 2.10,
-                TotaltAntallAksjer = 3000,
-                Boers = "New York Børs"
-            };
-
-            var aksjeMicrosoft = new Aksje
-            {
-                Id = 2,
-                Aksjenavn = "Microsoft",
-                Ticker = "MICR",
-                Markedspris = 8.00,
-                TotaltAntallAksjer = 11500,
-                Boers = "Oslo Børs"
-            };
-
-            var aksjeMeta = new Aksje
-            {
-                Id = 3,
-                Aksjenavn = "Meta Inc.",
-                Ticker = "META",
-                Markedspris = 40.00,
-                TotaltAntallAksjer = 1000500,
-                Boers = "San Fran Børs"
-            };
-            
-            var aksjeCocaCola = new Aksje
-            {
-                Id = 4,
-                Aksjenavn = "Coca Cola",
-                Ticker = "COCA",
-                Markedspris = 138.20,
-                TotaltAntallAksjer = 8900400,
-                Boers = "Oslo børs"
-            };
-            
-            _alleAksjer.Add(aksjeApple);
-            _alleAksjer.Add(aksjeMicrosoft);
-            _alleAksjer.Add(aksjeMeta);
-            _alleAksjer.Add(aksjeCocaCola);
-            _alleAksjer.Add(aksjeCocaCola);
-            _alleAksjer.Add(aksjeCocaCola);
-            /*
-            _alleAksjer.Add(aksjeCocaCola);
-            _alleAksjer.Add(aksjeCocaCola);
-            _alleAksjer.Add(aksjeCocaCola);
-            _alleAksjer.Add(aksjeCocaCola);
-            
-
-
-            return _alleAksjer;
-        }
-    }
-}
-*/
