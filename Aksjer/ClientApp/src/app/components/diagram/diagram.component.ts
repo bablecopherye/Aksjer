a4df2373f3cb3f06af02740a6ad15a0cfec54a02
Alexander Hagen Huse <ahh-produktivitet@tutanota.com>
2022-11-27 15:14:59 +0100

    Første skisse til implementasjon av diagram i min aksjebeldning-sida

--- Aksjer/ClientApp/src/app/components/diagram/diagram.component.ts
+++ Aksjer/ClientApp/src/app/components/diagram/diagram.component.ts
@@ -1,13 +1,26 @@
 ﻿import { Component } from "@angular/core";
+import {ApexChart, ApexNonAxisChartSeries, NgApexchartsModule} from "ng-apexcharts";
+import { OnInit} from "@angular/core";
 
 @Component({
     selector: 'diagram',
     templateUrl: './diagram.component.html',
     styleUrls: ['./diagram.component.css']
 })
-export class DiagramComponent {
+export class DiagramComponent implements OnInit{
+    
+    public chartSeries: ApexNonAxisChartSeries = [40, 32, 28, 55];
+    
+    public chartDetails: ApexChart = {
+        type: 'pie',
+        toolbar: {
+            show: true
+        }
+    };
 
     constructor() {
     }
 
+    ngOnInit() {
+    }
 }
