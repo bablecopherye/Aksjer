037f330fc233f8416c59a5e8235994f7d84d861a
Alexander Hagen Huse <ahh-produktivitet@tutanota.com>
2022-11-27 13:19:01 +0100

    Opprettet en diagram-komponenet, som skal kunne vise sammensetningen av aksjeporteføljen under siden Mine aksjer.

---
+++ Aksjer/ClientApp/src/app/components/diagram/diagram.component.ts
@@ -0,0 +1,13 @@
+﻿import { Component } from "@angular/core";
+
+@Component({
+    selector: 'diagram',
+    templateUrl: './diagram.component.html',
+    styleUrls: ['./diagram.component.css']
+})
+export class DiagramComponent {
+
+    constructor() {
+    }
+
+}
