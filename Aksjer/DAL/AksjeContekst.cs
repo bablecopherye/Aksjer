768d47035e777ef129e90e9170ae94a58c5576ee
Alexander Hagen Huse <ahh-produktivitet@tutanota.com>
2022-11-28 12:47:01 +0100

    Oppdaterte Bruker og Ordre-klassene i AksjeContekst.cs

--- Aksjer/DAL/AksjeContekst.cs
+++ Aksjer/DAL/AksjeContekst.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using Aksjer.DAL;
 using Aksjer.Models;
 
 namespace Aksjer.DAL
@@ -30,11 +32,34 @@ namespace Aksjer.DAL
 
     public class Brukere
     {
-        public int Id { get; set; }
         public string Brukernavn { get; set; }
         public byte[] Passord { get; set; }
         public byte[] Salt { get; set; }
+        public string Fornavn { get; set; }
+        public string Etternavn { get; set; }
+        public double Saldo { get; set; }
+        public List<Ordre> Ordre { get; set; }
+        public Aksjebeholdning Aksjebeholdning { get; set; }
+        
     }
+    
+    public class Ordrer
+    {
+        public int Id { get; set; }
+        public string DatoAar { get; set; }
+        public string DatoMnd { get; set; }
+        public string DatoDag { get; set; }
+        public string TidTime { get; set; }
+        public string TidMinutt { get; set; }
+        public string TidSekund { get; set; }
+        virtual public Aksje Aksje { get; set; }
+        public string Type { get; set; }
+        public int Antall { get; set; }
+        public double Pris { get; set; }
+        virtual public Bruker Kunde { get; set; }
+    }
+}
+    
 
     public class AksjeContext : DbContext
     {
@@ -46,11 +71,12 @@ namespace Aksjer.DAL
         public DbSet<Aksje> Aksjer { get; set; }
         public DbSet<Personer> Personer { get; set; }
         public DbSet<Bruker> Brukere { get; set; }
+        public DbSet<Ordre> Ordrer { get; set; }
 
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseLazyLoadingProxies();
         }
-    }
 }
+
