156f0eb22c5d61bc50249e54f8f31a522cb7d9e2
Alexander Hagen Huse <ahh-produktivitet@tutanota.com>
2022-11-28 13:26:24 +0100

    Strukturerte DBInit.cs

--- Aksjer/DAL/DBInit.cs
+++ Aksjer/DAL/DBInit.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reflection.Metadata.Ecma335;
+using System.Runtime.CompilerServices;
 using Aksjer.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
@@ -15,8 +16,21 @@ namespace Aksjer.DAL
 
                 db.Database.EnsureDeleted();
                 db.Database.EnsureCreated();
+                
+                ////// ----- BRUKERE ----- /////////////////////////////////////////////////////////////////////////////
+                
+                // lag en påoggingsbruker
+                var bruker = new Bruker();
+                bruker.Brukernavn = "Admin";
+                string passord = "Test11";
+                byte[] salt = AksjeRepository.LagSalt();
+                byte[] hash = AksjeRepository.LagHash(passord, salt);
+                bruker.Passord = hash;
+                bruker.Salt = salt;
+                
+                db.Brukere.Add(bruker);
 
-                ////// Opprett aksjer og legg til aksjer i databasen ved oppstart //////////////////////////////////////
+                ////// ----- AKSJER ----- //////////////////////////////////////////////////////////////////////////////
                 var aksjeApple = new Aksje() { Id = 1, Ticker = "APPL", Aksjenavn = "Apple", Pris = 450.00, Antall = 15000000, Bors = "New York", Land = "USA"};
                 var aksjeMeta = new Aksje() { Id = 2, Ticker = "META", Aksjenavn = "Meta", Pris = 58.72, Antall = 24310340, Bors = "New York", Land = "USA"};
                 var aksjeEquinor = new Aksje() { Id = 3, Ticker = "EQN", Aksjenavn = "Equinor", Pris = 230.20, Antall = 530050609, Bors = "Oslo Børs", Land = "Norge"};
@@ -31,20 +45,17 @@ namespace Aksjer.DAL
                 db.Aksjer.Add(aksjeNovoNordisk);
                 db.Aksjer.Add(aksjeTesla);
                 
-                ////// Opprett aksjer og legg til påloggingsbrukere i databasen ved oppstart ///////////////////////////
-                var person1 = new Personer { Fornavn = "Ole", Etternavn = "Hansen" };
-                var person2 = new Personer { Fornavn = "Line", Etternavn = "Jensen" };
+                ////// ----- TIDLIGERE ORDRE ----- /////////////////////////////////////////////////////////////////////
+                var ordreHansLarsen1 = new Ordre() { 
+                    Id = 1, DatoAar = "2022", DatoMnd = "07", DatoDag = "23", TidTime = "11", TidMinutt = "58", TidSekund = "31", 
+                    Type = "Kjøp", Aksje = aksjeApple, Antall = 400, Pris = 156000, Kunde = "Hans Larsen"};
+                
+                
+                ////// ----- AKSJEBEHOLDNING ----- /////////////////////////////////////////////////////////////////////
+                
+                
 
-                /////// lag en påoggingsbruker
-                var bruker = new Brukere();
-                bruker.Brukernavn = "Admin";
-                string passord = "Test11";
-                byte[] salt = AksjeRepository.LagSalt();
-                byte[] hash = AksjeRepository.LagHash(passord, salt);
-                bruker.Passord = hash;
-                bruker.Salt = salt;
-                db.Brukere.Add(bruker);
-
+                ////// ----- LAGRE TIL DATABASE ----- //////////////////////////////////////////////////////////////////
                 db.SaveChanges();
             }
         }
