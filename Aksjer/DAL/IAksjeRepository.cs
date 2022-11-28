e72e2d815a95f64fe4cd07bd8977db9d3f7642ce
Alexander Hagen Huse <ahh-produktivitet@tutanota.com>
2022-11-29 00:15:03 +0100

    Kommentert ut kode som ikke skal brukes i AksjeRepository.cs, og endret noen metodenavn

--- Aksjer/DAL/IAksjeRepository.cs
+++ Aksjer/DAL/IAksjeRepository.cs
@@ -6,11 +6,11 @@ namespace Aksjer.DAL
 {
     public interface IAksjeRepository
     {
-        Task<bool> Lagre(Aksje innAksje);
-        Task<List<Aksje>> HentAlle();
-        Task<bool> Slett(int id);
-        Task<Aksje> HentEn(int id);
-        Task<bool> Endre(Aksje endreAksje);
-        Task<bool> LoggInn(Bruker bruker);
+        // Task<bool> Lagre(Aksje innAksje); // Trenger ikke
+        Task<List<Aksje>> HentAlleAksjene();
+        // Task<bool> Slett(int id); // Trenger ikke
+        Task<Aksje> HentEnAksje(string ticker);
+        Task<bool> EndreAntalletTilgjengeligeAksjerIEnAksje(Aksje endreAksje);
+        // Task<bool> LoggInn(Bruker bruker); // Trenger ikke
     }
 }
