0ec62562aa5684801a6f33d1ab168dfb27047c97
Alexander Hagen Huse <ahh-produktivitet@tutanota.com>
2022-11-29 01:27:28 +0100

    Endret logg-inn-komponenten fra å ha modal i navnet til å være en vanlig side

---
+++ Aksjer/ClientApp/src/app/components/logg-inn/logg-inn.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit} from "@angular/core";
+
+@Component({
+    selector: 'logg-inn',
+    templateUrl: './logg-inn.component.html',
+    styleUrls: ['./logg-inn.component.css']
+})
+export class LoggInnComponent {
+
+    constructor() {}
+
+}
