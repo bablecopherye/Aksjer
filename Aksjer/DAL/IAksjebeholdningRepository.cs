800f3ee0a7b7aa23215bc7988bb9fa49bcc1c7a0
Alexander Hagen Huse <ahh-produktivitet@tutanota.com>
2022-11-29 09:13:47 +0100

    Første kladd av IAksjebeholdningRepository.cs

--- Aksjer/DAL/IAksjebeholdningRepository.cs
+++ Aksjer/DAL/IAksjebeholdningRepository.cs
@@ -1,7 +1,15 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Aksjer.Models;
+
 namespace Aksjer.DAL
 {
     public class IAksjebeholdningRepository
     {
-        
+        Task<bool> LagreNyAksjeTilBeholdningen(Aksje innAksje);
+        Task<List<Aksje>> HentAlleAksjeneIBeholdningen();
+        Task<bool> SlettAksjeHvisDuSelgerAlt(int id);
+        Task<Aksje> HentEnAksje(string ticker);
+        Task<bool> EndreAntalletEideAksjerIEnAksje(Aksje endreAksje);
     }
 }
