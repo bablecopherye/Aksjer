8dcf333a0517d116ecf4157bea0ee4ae6a002b16
Alexander Hagen Huse <ahh-produktivitet@tutanota.com>
2022-11-29 15:08:30 +0100

    Lagt til tittelkommentarer for bedre oversikt, og fjernet utkommentering av en del felt

--- Aksjer/DAL/AksjeContekst.cs
+++ Aksjer/DAL/AksjeContekst.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Aksjer.DAL;
@@ -6,6 +7,8 @@ using Aksjer.Models;
 
 namespace Aksjer.DAL
 {
+
+////////// ----- AKSJER ----- //////////////////////////////////////////////////////////////////////////////////////////    
     public class Aksjer
     {
         [Key]
@@ -15,24 +18,35 @@ namespace Aksjer.DAL
         public int Antall { get; set; }
         public string Bors { get; set; }
         public string Land { get; set; }
-
-        // virtual public Brukere Bruker { get; set; }
     }
 
+////////// ----- BRUKERE ----- //////////////////////////////////////////////////////////////////////////////////    
     public class Brukere
     {
         [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public string Brukernavn { get; set; }
         public byte[] Passord { get; set; }
         public byte[] Salt { get; set; }
         public string Fornavn { get; set; }
         public string Etternavn { get; set; }
         public double Saldo { get; set; }
-        // public List<Ordre> Ordre { get; set; }
-        // public Aksjebeholdning Aksjebeholdning { get; set; }
+        public virtual List<Ordre> Ordre { get; set; }
+        public virtual List<Aksjebeholdning> Aksjebeholdning { get; set; }
     }
     
+////////// ----- AKSJEBEHOLDNINGER ----- ///////////////////////////////////////////////////////////////////////////////  
+    public class Aksjebeholdninger
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public virtual Bruker Bruker { get; set; }
+        public virtual Aksje Aksje { get; set; }
+        public int AntallAksjerEid { get; set; }
+        public double Kostpris { get; set; }
+    }    
+
+
+////////// ----- ORDRER ----- //////////////////////////////////////////////////////////////////////////////////////////    
     public class Ordrer
     {
         [Key]
@@ -44,15 +58,15 @@ namespace Aksjer.DAL
         public string TidTime { get; set; }
         public string TidMinutt { get; set; }
         public string TidSekund { get; set; }
-        // virtual public Aksje Aksje { get; set; }
+        virtual public Aksje Aksje { get; set; }
         public string Type { get; set; }
         public int Antall { get; set; }
         public double Pris { get; set; }
         virtual public Brukere Kunde { get; set; }
     }
 }
-    
 
+////////// ----- OPPRETT DATABASE ----- ////////////////////////////////////////////////////////////////////////////////
     public class AksjeContext : DbContext
     {
         public AksjeContext(DbContextOptions<AksjeContext> options) : base(options)
