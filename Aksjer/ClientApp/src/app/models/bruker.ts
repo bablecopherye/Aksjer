a563816da9f8af2ce46f501e4f538622d6bf975d
Alexander Hagen Huse <ahh-produktivitet@tutanota.com>
2022-11-29 06:17:58 +0100

    Gjorde noen attributter valgfire

--- Aksjer/ClientApp/src/app/models/bruker.ts
+++ Aksjer/ClientApp/src/app/models/bruker.ts
@@ -1,7 +1,7 @@
 export interface IBruker {
     brukernavn: string,
     passord: string,
-    fornavn: string,
-    etternavn: string,
-    saldo: number
+    fornavn?: string,
+    etternavn?: string,
+    saldo?: number
 }
