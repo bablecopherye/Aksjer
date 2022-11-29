8d218220d25b7dd2fb324d51896e8ee8da854a42
Alexander Hagen Huse <ahh-produktivitet@tutanota.com>
2022-11-29 19:48:01 +0100

    Småjusteringer av Aksjebeholdning.cs

--- Aksjer/Models/Aksjebeholdning.cs
+++ Aksjer/Models/Aksjebeholdning.cs
@@ -1,10 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Aksjer.Models
 {
     public class Aksjebeholdning
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public int Id { get; set; }
-        public Bruker Bruker { get; set; }
-        public Aksje Aksje { get; set; }
+        public virtual Bruker Bruker { get; set; }
+        public virtual Aksje Aksje { get; set; }
         public int AntallAksjerEid { get; set; }
         public double Kostpris { get; set; }
     }
